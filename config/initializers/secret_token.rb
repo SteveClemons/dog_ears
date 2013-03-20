274a573dc28e35635447a41ad9d5771e1f51d24d
SteveClemons <clemons@madisoncollege.edu>
2013-03-20 12:55:49 -0500

    Initial Commit Project 2

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DogEars::Application.config.secret_token = 'e732f7844429af34f027935c83a0894e46b90bd84c77e47e1fd9cc48b146ca02db0e184cb0b0bb998fe462019ff58eb3245308dc6f2d9fc3a75a79098a265614'
